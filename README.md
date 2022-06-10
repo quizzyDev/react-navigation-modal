cdf81a7055680394b1134d8141a59dd21ad1798e
quizzyDev <quizzy.dev@gmail.com>
2022-06-10 09:45:23 +0100

    fix: Add initial modules and readme.ms

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Navigation Modal
+
+### About this repo
+
+This repo is a boiler plate expo typscript app that makes use of [`react-navigation`](https://reactnavigation.org/) and [`react-native-reanimated`](https://docs.swmansion.com/react-native-reanimated/docs/) to handle `Modal` windows.
+
+The project has been configured to use aliases for easy importing using the [`babel-plugin-module-resolver`](https://github.com/tleunen/babel-plugin-module-resolver) and has an opinionated file structure for separating components, screens and utils etc.
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          alias: {
+            "~/": "./",
+            "~/components": "./src/components",
+            "~/constants": "./src/constants",
+            "~/screens": "./src/screens",
+            "~/modals": "./src/modals",
+            "~/navigators": "./src/navigators",
+            "~/styles": "./src/styles",
+            "~/hooks": "./src/hooks",
+            "~/utils": "./src/utils",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
